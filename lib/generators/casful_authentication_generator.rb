ec7ba7403ead33a98ad32cfa72b860d1ea81797b
Austen Ott <ottai@uwec.edu>
2012-02-16 14:21:24 -0600

    updated the generator to use the rubycas-client gem instead of the plugin

--- lib/generators/casful_authentication_generator.rb
+++ lib/generators/casful_authentication_generator.rb
@@ -6,13 +6,12 @@ class CasfulAuthenticationGenerator < Rails::Generators::NamedBase
   attr_reader :cas_server
   source_root File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
   argument :cas_server, :required => true, :desc => "The URL of the CAS server to authenticate through"
-  class_option :skip_plugin, :type => :boolean, :default => false, :desc => "Skip installing the rubycas-client plugin"
+  class_option :skip_gem, :type => :boolean, :default => false, :desc => "Skip installing the rubycas-client plugin"
   
   def manifest
-    unless options[:skip_plugin]
-      puts "Installing the Cas plugin from"
-      puts "git://github.com/gunark/rubycas-client.git"
-      plugin("rubycas-client", :git => "git://github.com/gunark/rubycas-client.git")
+    unless options[:skip_gem]
+      puts "Installing the rubycas-client gem"
+      gem("rubycas-client")
     end
     template "cas_authentication.rb", "lib/cas_authentication.rb"
     template "cas.rb", "config/initializers/cas.rb"
